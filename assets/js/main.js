29e2e84e128c63b0fdfc9fe1e3caeef60d826957
francescomascellino <franesco.mascellino@gmail.com>
2023-08-31 16:41:55 +0200

    add reverse word with for loop function

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,30 +91,50 @@ evenOddForm.addEventListener("submit", function (e) {
 
     }
 
-})
+});
 
 /* Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
 //iniziare a leggere la stringa dal suo valore massimo di lunghezza  e sottrarre man mano all'indice fino ad arrivare a 0 e aggiungere a una nuova variabile i vari valori dell'indice per ottenere la stringa al contrario.
 
-let word = "casa";
-let reverseWord;
+function reverse(wordToCheck) {
 
-for (let i = word.length - 1; i >= 0; i--) {
+    let reverseWord = "";
 
-    console.log(word[i]);
-    reverseWord += word[i]
+    console.log(wordToCheck.length)
+
+    for (let i = wordToCheck.length - 1; i >= 0; i--) {
+
+        reverseWord += wordToCheck[i]
+
+    };
+
+    return reverseWord
 
 }
 
-console.log(reverseWord);
+let word = "anna"
+let reverseResult = reverse(word);
+console.log(reverseResult);
 
-let wordToArray = word.split("")
-console.log("word to array =", wordToArray);
+if (word == reverseResult) {
+    console.log("palindromo");
+}
 
-let wordToArrayReverse = wordToArray.reverse()
-console.log("reverse word to array =", wordToArrayReverse);
+/* WITH ARRAY AND reverse()
 
-let newWord = wordToArrayReverse.join("")
-console.log("new word =", newWord);
+function reverse(userImput) {
+
+    let wordToArray = userImput.split("")
+    console.log("word to array =", wordToArray);
+
+    let wordToArrayReverse = wordToArray.reverse()
+    console.log("reverse word to array =", wordToArrayReverse);
+
+    let newWord = wordToArrayReverse.join("")
+    console.log("new word =", newWord);
+
+};
+
+*/
