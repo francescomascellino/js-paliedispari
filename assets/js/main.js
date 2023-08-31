0762cfb69c8ee251d58dff8d01c9fcddddb2af9d
francescomascellino <franesco.mascellino@gmail.com>
2023-08-31 14:28:14 +0200

    add markup and switch

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,8 @@
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-
-
 const randomNum = (Math.floor(Math.random(1, 5) * 5) + 1);
-console.log(randomNum);
+console.log("numero random =", randomNum);
 
 const userNum = 5;
 
@@ -19,13 +17,39 @@ function sumEvenOdd(value1, value2) {
 
 }
 
-if (sumEvenOdd(randomNum, userNum)) {
-    console.log("pari");
-} else {
-    console.log("dispari");
-}
+const confirmSelbtn = document.getElementById("confirmEvenOdd");
+const evenOddChoice = document.getElementById("evenOddChoice");
 
+confirmSelbtn.addEventListener("click", function () {
+    const userChoice = evenOddChoice.value;
+    console.log("scelta utente =", userChoice);
 
+    if (sumEvenOdd(randomNum, userNum)) {
+
+        switch (userChoice) {
+            case "even":
+                console.log("user wins");
+                break;
+
+            default:
+                console.log("user loses");
+                break;
+        }
+
+    } else {
+
+        switch (userChoice) {
+            case "odd":
+                console.log("user wins");
+                break;
+
+            default:
+                console.log("user loses");
+                break;
+        };
+    }
+
+})
 
 /* Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
